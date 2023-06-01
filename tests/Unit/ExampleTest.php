ffaec168a793c3bd42d7875f418a223f8d18310e
UserHab <vitalibembel1.21@gmail.com>
2023-06-02 01:07:32 +0300

    12 - unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,73 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\RegFormController;
+use App\Models\Employee;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function testRegistration()
     {
-        $this->assertTrue(true);
+        
+        $request = new Request([
+            'last_name' => 'Doe',
+            'first_name' => 'John',
+            'patronymic' => 'Alex',
+            'company' => 'Test Company',
+            'department' => 'Test Department',
+        ]);
+        
+        $controller = new RegFormController();
+  
+        $response = $controller->registration($request);
+
+        $response->assertRedirect(route('index'));
+
+        $this->assertDatabaseHas('employees', [
+            'lastname' => 'Doe',
+            'firstname' => 'John',
+            'patronymic' => 'Alex',
+            'company' => 'Test Company',
+            'department' => 'Test Department',
+        ]);
+    }
+
+    public function testRegistrationWithInvalidData()
+    {
+        $request = new Request([
+            'last_name' => '',
+            'first_name' => '',
+            'patronymic' => '',
+            'company' => '',
+            'department' => '',
+        ]);
+        
+        $controller = new RegFormController();
+
+        $response = $controller->registration($request);
+
+        $response->assertRedirect();
+        $response->assertSessionHasErrors([
+            'last_name',
+            'first_name',
+            'patronymic',
+            'company',
+            'department',
+        ]);
+
+        $this->assertDatabaseMissing('employees', [
+            'lastname' => '',
+            'firstname' => '',
+            'patronymic' => '',
+            'company' => '',
+            'department' => '',
+        ]);
     }
 }
